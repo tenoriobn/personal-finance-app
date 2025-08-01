797d6363e25c7a87c527658ea8c3b1a7d7227b00
Bruno Tenorio <tenoriobn@gmail.com>
2025-08-01 13:24:03 -0300

    feat: :sparkles: 01/08/25 - cria ui do input e dropdown para filtrar dados da tabela de 'transactions'

---
+++ app/components/Input/input.type.ts
@@ -0,0 +1,9 @@
+import type { Component } from 'vue';
+
+export interface InputProps {
+  label: string
+  modelValue: string
+  icon: Component | string
+  name: string
+  customClasses?: string
+}
---
+++ app/screens/Transactions/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid max-md:gap-8 md:gap-10">
+    <h2 class="text-grey-900 text-[2rem] font-bold leading-none">Transactions</h2>
+
+    <div class="bg-white rounded-xl h-full max-md:p-4 md:p-10 w-full overflow-hidden">
+      <Filter />
+      <Table />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Table from './Table.vue';
+import Filter from './Filter/index.vue';
+</script>
