073ff0efc9969da4b406e3b93b3c81bd519c60f3
Bruno Tenorio <tenoriobn@gmail.com>
2025-08-19 16:28:10 -0300

    feat: :sparkles: 19/08/25 - cria base para página de Orçamentos

--- app/pages/orcamentos.vue
+++ app/pages/orcamentos.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="h-full">
-    <h1 class="text-amber-600">Welcome to the orçamentos</h1>
-    <h2>deu certo</h2>
-  </div>
+  <Orcamentos />
 </template>
+
+<script lang="ts" setup>
+import Orcamentos from '~/screens/Orcamentos/index.vue';
+</script>
