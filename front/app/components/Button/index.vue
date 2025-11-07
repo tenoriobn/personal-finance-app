a425af2aa30db9153cccc7e1bc6c72452cc01774
Bruno Tenorio <tenoriobn@gmail.com>
2025-11-07 16:06:14 -0300

    refactor: :recycle: 07/11/25 - simplifica estilos para estados com/sem submit

--- front/app/components/Button/index.vue
+++ front/app/components/Button/index.vue
@@ -1,7 +1,12 @@
 <template>
   <button
     :disabled="isSubmitting"
-    class="flex items-center justify-center gap-2 border border-grey-900 bg-grey-900 max-sm:text-sm text-sm text-grey-200 stroke-grey-200 font-medium hover:bg-grey-500 hover:border-grey-500 hover:text-grey-200 active:bg-grey-300 active:border-grey-300  active:text-grey-500 active:stroke-grey-500 p-3 rounded-xl duration-150 ease-in-out shadow-lg shadow-grey-500 hover:shadow-grey-300 active:shadow-sm min-w-28 disabled:cursor-not-allowed disabled:hover:bg-grey-900 disabled:hover:border-grey-900 disabled:hover:text-grey-200 disabled:active:bg-grey-900 disabled:active:border-grey-900 disabled:active:text-grey-200 disabled:shadow-none"
+    :class="[
+      'flex items-center justify-center gap-2 border border-grey-900 bg-grey-900 max-sm:text-sm text-sm text-grey-200 stroke-grey-200 font-medium p-3 rounded-xl duration-150 ease-in-out  min-w-28',
+      isSubmitting
+        ? 'disabled:cursor-not-allowed'
+        : 'shadow-lg shadow-grey-500 hover:bg-grey-500 hover:border-grey-500 hover:text-grey-200 active:bg-grey-300 active:border-grey-300  active:text-grey-500 active:stroke-grey-500 hover:shadow-grey-300 active:shadow-sm ',
+    ]"
   >
     <LoadingIcon
       v-if="isSubmitting"
