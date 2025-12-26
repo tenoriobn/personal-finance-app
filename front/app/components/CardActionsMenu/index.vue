607faafe6fecc62a35fd48717622a9ae6d17f57f
Bruno Tenorio <tenoriobn@gmail.com>
2025-12-26 16:14:33 -0300

    feat: :sparkles: 26/12/2025 - Adiciona testes para o componente CardActionsMenu e aprimora o HTML semântico.

--- front/app/components/CardActionsMenu/index.vue
+++ front/app/components/CardActionsMenu/index.vue
@@ -4,7 +4,12 @@
     class="relative grid"
   >
     <button
+      type="button"
       class="opacity-50 hover:opacity-100 duration-150 ease-in-out relative"
+      aria-haspopup="menu"
+      :aria-expanded="isOpen"
+      aria-label="Abrir menu de ações"
+      data-testid="actions-toggle"
       @click="toggleBudgetActions"
     >
       <DotsIcon />
@@ -13,25 +18,34 @@
     <AnimatePresence>
       <motion.div
         v-if="isOpen"
+        role="menu"
         class="absolute bg-white border border-grey-200 right-0 mt-6 z-10 rounded-xl shadow-md overflow-hidden w-max max-w-[156px]"
         v-bind="fadeSlideY"
       >
-        <ul
-          class="max-h-[212px] overflow-y-auto scroll-hide relative"
-        >
-          <button
-            class="text-grey-900 hover:text-grey-500 active:text-grey-300 text-sm font-medium p-4 cursor-pointer duration-150 ease-in-out border-b border-grey-100 last:border-b-0 w-full"
-            @click="editBudget"
-          >
-            {{ editLabel }}
-          </button>
+        <ul class="max-h-[212px] overflow-y-auto scroll-hide relative">
+          <li>
+            <button
+              type="button"
+              role="menuitem"
+              data-testid="edit-action"
+              class="text-grey-900 hover:text-grey-500 active:text-grey-300 text-sm font-medium p-4 w-full text-left duration-150 ease-in-out border-b border-grey-100"
+              @click="editBudget"
+            >
+              {{ editLabel }}
+            </button>
+          </li>
 
-          <button
-            class="text-red hover:text-red/85 active:text-red/55 text-sm font-medium p-4 cursor-pointer duration-150 ease-in-out border-b border-grey-100 last:border-b-0 w-full"
-            @click="deleteBudget"
-          >
-            {{ deleteLabel }}
-          </button>
+          <li>
+            <button
+              type="button"
+              role="menuitem"
+              data-testid="delete-action"
+              class="text-red hover:text-red/85 active:text-red/55 text-sm font-medium p-4 w-full text-left duration-150 ease-in-out"
+              @click="deleteBudget"
+            >
+              {{ deleteLabel }}
+            </button>
+          </li>
         </ul>
       </motion.div>
     </AnimatePresence>
