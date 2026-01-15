ea75a84c8b31ab5a4bfd6a279f6cca38499a1552
Bruno Tenorio <tenoriobn@gmail.com>
2026-01-15 16:54:08 -0300

    refactor: :recycle: 15/01/26 - atualiza cors e readme com novos dados

--- back/src/middleware/cors.ts
+++ back/src/middleware/cors.ts
@@ -1,21 +1,20 @@
 import { cors } from "hono/cors";
 
 const allowedOrigins = [
-  "http://localhost:3000",
-  "https://personal-finance-app-rosy.vercel.app",
-  /^https:\/\/personal-finance-[a-z0-9-]+\.vercel\.app$/
+  "https://app-personal-finance.vercel.app/",
+  /^https:\/\/app-personal-finance-[a-z0-9-]+\.vercel\.app$/
 ];
 
-const vercelPreviewRegex = /^https:\/\/personal-finance-[a-z0-9-]+\.vercel\.app$/;
+const vercelPreviewRegex = /^https:\/\/app-personal-finance-[a-z0-9-]+\.vercel\.app$/;
 
 export const corsMiddleware = cors({
   origin: (origin) => {
     if (!origin) { return null; }
 
-    // const isDev = process.env.NODE_ENV === "development";
-    // if (isDev) {
-    //   allowedOrigins.push("http://localhost:3000");
-    // }
+    const isDev = process.env.NODE_ENV === "development";
+    if (isDev) {
+      allowedOrigins.push("http://localhost:3000");
+    }
 
     if (allowedOrigins.includes(origin)) {
       return origin;
